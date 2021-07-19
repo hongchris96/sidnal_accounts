96fa626dce61eb685bd1a0b5b850e5e28eaa3796
Chris Hanma <hongchris96@hotmail.com>
2021-07-19 15:53:45 -0700

    Add Account model validations and associations

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,13 @@
 class Account < ApplicationRecord
+  validates :acc_id, :email, presence: true, uniqueness: true
+  validates :balance, :picture, :credit, :name_first, :name_last, :employer, :address, :comments, :created, presence: true
+  validates :phone, presence: true, uniqueness: true, length: {is: 10}
+
+  has_many :account_tags_joins,
+    foreign_key: :account_id,
+    class_name: :AccountTag
+
+  has_many :account_tags,
+    through: :account_tags_joins,
+    source: :tags
 end
