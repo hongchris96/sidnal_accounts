18b538222608db289374f20779f831f5f7de4736
Chris Hanma <hongchris96@hotmail.com>
2021-07-19 15:56:52 -0700

    Add Joins Table validations and associations

--- app/models/account_tag.rb
+++ app/models/account_tag.rb
@@ -1,2 +1,12 @@
 class AccountTag < ApplicationRecord
+  validates :account_id, :tag_id, presence: true
+  validates :account_id, uniqueness: {scope: :tag_id}
+
+  belongs_to :accounts,
+    foreign_key: :account_id,
+    class_name: :Accounts
+
+  belongs_to :tags,
+    foreign_key: :tag_id,
+    class_name: :Tags
 end
