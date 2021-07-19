ed33e7462ed7fc49b117fcf8cb86f60f1fbacf77
Chris Hanma <hongchris96@hotmail.com>
2021-07-19 15:59:23 -0700

    Add Tag model validations and associations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,11 @@
 class Tag < ApplicationRecord
+  validates :tag_name, presence: true, uniqueness: true
+
+  has_many :account_tags_joins,
+    foreign_key: :tag_id,
+    class_name: :AccountTag
+
+  has_many :tagged_accounts,
+    through: :account_tags_joins,
+    source: :accounts
 end
