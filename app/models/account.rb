82cf30f62cb7fb5e19fdc0a388e5c6cd10978982
Chris Hanma <hongchris96@hotmail.com>
2021-07-19 18:57:48 -0700

    Remove phone validations in account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,7 @@
 class Account < ApplicationRecord
   validates :acc_id, :email, presence: true, uniqueness: true
   validates :balance, :picture, :credit, :name_first, :name_last, :employer, :address, :comments, :created, presence: true
-  validates :phone, presence: true, uniqueness: true, length: {is: 10}
+  validates :phone, presence: true, uniqueness: true
 
   has_many :account_tags_joins,
     foreign_key: :account_id,
