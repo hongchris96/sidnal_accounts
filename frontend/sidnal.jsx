a051fc1be03b5a9b6fbbf5f311064d5cdc12a144
Chris Hanma <hongchris96@hotmail.com>
2021-07-19 19:15:20 -0700

    Update entry file

---
+++ frontend/sidnal.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+
+  let preloadedState = undefined;
+
+  const store = configureStore(preloadedState);
+
+  // testing
+  window.store = store;
+
+  ReactDOM.render(<Root store={store}/>, root);
+});
