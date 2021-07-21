26563b19978b2673fccc526510d58cf39735ae07
Chris Hanma <hongchris96@hotmail.com>
2021-07-20 17:24:36 -0700

    Add mortgage eligibility to dashboard

--- frontend/components/dashboard_acc.jsx
+++ frontend/components/dashboard_acc.jsx
@@ -7,6 +7,20 @@ class DashboardAccount extends React.Component {
   }
 
   render(){
+    let percentBalance = parseFloat(this.props.account.balance) / 5000;
+    if (percentBalance > 1) percentBalance = 1;
+    let percentCredit = this.props.account.credit / 620;
+    if (percentCredit > 1) percentCredit = 1;
+    let howCloseToMorg = Math.floor((percentCredit * 0.5 + percentBalance * 0.5) * 100);
+    let shade = "red";
+    if (howCloseToMorg >= 80) {
+      shade = "rgb(51, 238, 23)";
+    } else if (howCloseToMorg >= 70) {
+      shade = "yellow";
+    } else if (howCloseToMorg >= 60) {
+      shade = "orange";
+    }
+
     return (
       <li className="account-card">
         <img src={`${this.props.account.picture}`} alt="account-picture"/>
@@ -14,6 +28,7 @@ class DashboardAccount extends React.Component {
           <h1>{this.props.account.name_first + " " + this.props.account.name_last}</h1>
           <p>{this.props.account.email}</p>
           <p>{this.props.account.phone}</p>
+          <p style={{color: `${shade}`}}>{howCloseToMorg}% towards Mortgage Eligible</p>
           <Link to={`/accounts/${this.props.account.id}`}>More Info</Link>
         </div>
       </li>
