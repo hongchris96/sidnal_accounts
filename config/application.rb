6374fa108e6916d8326c87a9d2ac9fe512423295
Chris Hanma <hongchris96@hotmail.com>
2021-07-20 20:32:43 -0700

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module SidnalAccounts
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.assets.enabled = true
     config.assets.initialize_on_precompile = false
   end
 end
